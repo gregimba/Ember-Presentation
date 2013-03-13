098f7308fcafb31f005011db5a7839f5caf73c97
Grant Regimbal <regimbal.grant@gmail.com>
2013-03-12 22:25:39 -0700

    init

---
+++ Random/starter-kit-final/server.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "Serving at http://localhost:5000"
+python -m SimpleHTTPServer 5000
---
+++ Router/server.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "Serving at http://localhost:5001"
+python -m SimpleHTTPServer 5001
